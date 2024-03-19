17787c2557ebe1e09d9d554e377db827a90a2756
Tom Howard <t.howard@sheffield.ac.uk>
2024-03-19 15:14:24 +0000

    proper shutdowns

--- tuos_examples/src/waffle_test.py
+++ tuos_examples/src/waffle_test.py
@@ -12,10 +12,17 @@ rospy.loginfo(f"{node_name}: Initialised.")
 motion = waffle.Motion(debug = True)
 lidar = waffle.Lidar(debug = True)
 
-motion.set_velocity(linear=0.1, angular=0.0)
+def stop():
+    motion.stop()
 
+rospy.on_shutdown(stop)
+
+motion.set_velocity(linear=0.05, angular=0.0)
 while not rospy.is_shutdown():
 
     print(lidar.distance)
     motion.publish_velocity()
-    rate.sleep()
+    try:
+        rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
