8884746c16a9df0cbb937cdb6f06f3a89488730f
Tom Howard <t.howard@sheffield.ac.uk>
2024-03-21 15:00:39 +0000

    move functions out of lidar callback

--- tuos_examples/src/waffle.py
+++ tuos_examples/src/waffle.py
@@ -140,36 +140,37 @@ class Lidar():
                 self.timestamp = rospy.get_time()
                 print(msg)
                         
+    def filter(self, lidar_subset):
+        valid_data = lidar_subset[(lidar_subset > 0.1) & (lidar_subset != float("inf"))]
+        subset_array = valid_data.tolist()
+        subset_value = valid_data.mean() if np.shape(valid_data)[0] > 0 else float("nan")
+        return subset_value, subset_array
+
+    def get_subset(self, start_index, stop_index):
+        subset = np.array(self.rangesArray[start_index: stop_index+1])
+        return self.filter(subset)
+    
     def laserscan_cb(self, scan_data: LaserScan):
         
         self.rangesArray = scan_data.ranges
 
-        def filter(get_subset):
-            valid_data = get_subset[(get_subset > 0.1) & (get_subset != float("inf"))]
-            subset_array = valid_data.tolist()
-            subset_value = valid_data.mean() if np.shape(valid_data)[0] > 0 else float("nan")
-            return subset_value, subset_array
-
-        def get_subset(start_index, stop_index):
-            range = np.array(scan_data.ranges[start_index: stop_index+1])
-            return filter(range)
-
-        # front:
+        ## Get LiDAR Subsets:
+        # Front:
         left = scan_data.ranges[0:20+1]
         right = scan_data.ranges[-20:]
         left_right = np.array(left[::-1] + right[::-1])
-        self.subsets.front, self.subsets.frontArray = filter(left_right)
+        self.subsets.front, self.subsets.frontArray = self.filter(left_right)
         
-        # right subsets:
-        self.subsets.r1, self.subsets.r1Array = get_subset(320, 340)
-        self.subsets.r2, self.subsets.r2Array = get_subset(300, 320)
-        self.subsets.r3, self.subsets.r3Array = get_subset(275, 290)
-        self.subsets.r4, self.subsets.r4Array = get_subset(250, 265)
+        # Right (x4):
+        self.subsets.r1, self.subsets.r1Array = self.get_subset(320, 340)
+        self.subsets.r2, self.subsets.r2Array = self.get_subset(300, 320)
+        self.subsets.r3, self.subsets.r3Array = self.get_subset(275, 290)
+        self.subsets.r4, self.subsets.r4Array = self.get_subset(250, 265)
         
-        # left subsets:
-        self.subsets.l1, self.subsets.l1Array = get_subset(20, 40)
-        self.subsets.l2, self.subsets.l2Array = get_subset(40, 60)
-        self.subsets.l3, self.subsets.l3Array = get_subset(70, 85)
-        self.subsets.l4, self.subsets.l4Array = get_subset(95, 110)
+        # Left (x4):
+        self.subsets.l1, self.subsets.l1Array = self.get_subset(20, 40)
+        self.subsets.l2, self.subsets.l2Array = self.get_subset(40, 60)
+        self.subsets.l3, self.subsets.l3Array = self.get_subset(70, 85)
+        self.subsets.l4, self.subsets.l4Array = self.get_subset(95, 110)
         
         self.wait_for_lidar = False
