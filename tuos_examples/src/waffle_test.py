4c536550e498d63fbeeba5505bb2f4484d65fe34
Tom Howard <t.howard@sheffield.ac.uk>
2024-03-21 15:19:21 +0000

    a (reasonably) simple usage example

--- tuos_examples/src/waffle_test.py
+++ tuos_examples/src/waffle_test.py
@@ -2,6 +2,7 @@
 
 import rospy
 import waffle
+from math import sqrt
 
 node_name = "waffle_py_example"
 
@@ -18,22 +19,41 @@ def stop():
 
 rospy.on_shutdown(stop)
 
-counter = 0
-inc = 1
+movement = "turn" # "move_fwd"
+transition = True
+
 while not rospy.is_shutdown():
 
-    rate.sleep()
-    counter += inc
-    if counter > 50:
-        inc = -1
-    elif counter < 0:
-        inc = 1
+    if transition:
+        motion.stop()
+        yaw_ref = odom.yaw
+        xpos_ref = odom.posx
+        ypos_ref = odom.posy
+        current_yaw = 0.0
+        current_distance = 0.0
+        transition = False
+    elif movement == "turn":
+        # turn
+        current_yaw += abs(odom.yaw - yaw_ref); yaw_ref = odom.yaw
+        if current_yaw > 45:
+            movement = "move_fwd"
+            transition = True
+        else:
+            motion.set_velocity(angular=0.2)
+            odom.show()
+    elif movement == "move_fwd":
+        current_distance += sqrt((odom.posx - xpos_ref)**2 + (odom.posy - ypos_ref)**2)
+        xpos_ref = odom.posx; ypos_ref = odom.posy
+        if current_distance > 0.5:
+            movement = "turn"
+            transition = True
+        else:
+            motion.set_velocity(linear=0.1)
+            lidar.subsets.show()   
     
-    if inc > 0:
-        motion.set_velocity(angular = 0.3)
-        odom.show()
-    elif inc < 0:
-        motion.set_velocity()
-        lidar.subsets.show()
-    
-    motion.publish_velocity()
+    if lidar.subsets.front < 0.5:
+        rospy.loginfo(f"Detected an object ahead at {lidar.subsets.front:.2f}m. Stopping!")
+        break
+    else:
+        motion.publish_velocity()
+        rate.sleep()
