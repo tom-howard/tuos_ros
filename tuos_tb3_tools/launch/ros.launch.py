ac1f1629783f8ae34e71b752810bddac95becf75
Tom Howard <t.howard@sheffield.ac.uk>
2024-10-08 14:01:30 +0100

    combined launch (working), without status node

--- tuos_tb3_tools/launch/ros.launch.py
+++ tuos_tb3_tools/launch/ros.launch.py
@@ -1,53 +1,79 @@
-import launch
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node  # Import for ROS 2 nodes
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, FindPackage
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     # Get package paths
-    turtlebot3_bringup_pkg = FindPackage('turtlebot3_bringup')
-    realsense2_camera_pkg = FindPackage('realsense2_camera')
-    tuos_tb3_tools_pkg = FindPackage('tuos_tb3_tools')
+    turtlebot3_bringup_pkg = get_package_share_directory('turtlebot3_bringup')
+    realsense2_camera_pkg = get_package_share_directory('realsense2_camera')
 
+    # tb3_status_node = Node(
+    #     package=tuos_tb3_tools_pkg,
+    #     executable='tb3_status.py',  # Assuming tb3_status.py is executable
+    #     name='tb3_status_node',
+    #     output='screen',
+    # )
+    
     # Launch arguments for realsense2_camera
-    output = LaunchConfiguration('output', default='log')
-    color_width = LaunchConfiguration('color_width', default='848')
-    color_height = LaunchConfiguration('color_height', default='480')
-    color_fps = LaunchConfiguration('color_fps', default='15')
-    align_depth = LaunchConfiguration('align_depth', default='true')
-
-    # Launch descriptions for each package
-    turtlebot3_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [turtlebot3_bringup_pkg, '/launch/turtlebot3_robot.launch.py']
-        )
+    output = LaunchConfiguration(
+        'output', default='log',
+    )
+    color_width = LaunchConfiguration(
+        'color_width', default='848',
+    )
+    color_height = LaunchConfiguration(
+        'color_height', default='480',
+    )
+    color_fps = LaunchConfiguration(
+        'color_fps', default='15'
+    )
+    align_depth = LaunchConfiguration(
+        'align_depth', default='true'
     )
 
-    realsense2_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [realsense2_camera_pkg, '/launch/rs_camera.launch.py']
+    return LaunchDescription([
+        
+        DeclareLaunchArgument(
+            'output',
+            default_value=output,
         ),
-        launch_arguments=[
-            output,
-            color_width,
-            color_height,
-            color_fps,
-            align_depth,
-        ]
-    )
+        DeclareLaunchArgument(
+            'color_width',
+            default_value=color_width,
+        ),
+        DeclareLaunchArgument(
+            'color_height',
+            default_value=color_height,
+        ),
+        DeclareLaunchArgument(
+            'color_fps',
+            default_value=color_fps,
+        ),
+        DeclareLaunchArgument(
+            'align_depth',
+            default_value=align_depth,
+        ),
 
-    tb3_status_node = Node(
-        package=tuos_tb3_tools_pkg,
-        executable='tb3_status.py',  # Assuming tb3_status.py is executable
-        name='tb3_status_node',
-        output='screen',
-    )
-
-    # Combine launch descriptions
-    return launch.LaunchDescription([
-        turtlebot3_launch,
-        realsense2_launch,
-        tb3_status_node,
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [turtlebot3_bringup_pkg, '/launch/robot.launch.py']
+            )
+        ),
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [realsense2_camera_pkg, '/launch/rs_launch.py']
+            ),
+            launch_arguments={
+                'output': output,
+                'color_width': color_width,
+                'color_height': color_height,
+                'color_fps': color_fps,
+                'align_depth': align_depth,
+            }.items(),
+        )
     ])
