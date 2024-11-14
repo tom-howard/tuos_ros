d03ef449034748865398747b461413d841b481da
Tom Howard <t.howard@sheffield.ac.uk>
2024-11-14 16:14:40 +0000

    status node working with tweaks still to be made

--- tuos_tb3_tools/scripts/tb3_status.py
+++ tuos_tb3_tools/scripts/tb3_status.py
@@ -3,17 +3,15 @@
 import os
 import sys
 import datetime as dt
-from turtlebot3_msgs.msg import Sound  # Assuming message type exists
-from sensor_msgs.msg import BatteryState  # Assuming message type exists
+from socket import gethostname
+from turtlebot3_msgs.srv import Sound
+from sensor_msgs.msg import BatteryState
 import rclpy
 from rclpy.node import Node
 from rclpy.signals import SignalHandlerOptions
-# from rclpy.qos import QoSProfile, QoSPreset
-# from rclpy.utilities import get_node_uri
 from rcl_interfaces.msg import Log  
 
-# Replace with your actual robot name
-# hostname = get_node_uri(use_localhost=False).split("/")[-1]  # Get robot name from URI
+hostname = gethostname()
 
 core_nodes = [
     'camera', 
@@ -29,21 +27,30 @@ class Tb3Status(Node):
     def __init__(self):
         super().__init__('tb3_status')
 
-        self.startup_complete = [False, False]
-        self.startup_sound = False
+        self.bringup_checks = [False, False, False]
+        self.bringup_complete = False
+        self.bringup_errors = False
         self.waffle_core_errors = False
         self.active_nodes = []
         self.shutdown = False
+        self.robot_status = ""
 
         self.rosout_sub = self.create_subscription(
             Log, '/rosout', self.rosout_cb, qos_profile=10)
 
-        self.create_timer(timer_period_sec=2, callback=self.timer_cb)
+        timer_rate = 1 # Hz
+        self.create_timer(timer_period_sec=1/timer_rate, callback=self.timer_cb)
+        self.timer_counter = 0
+        self.status_msg_trigger = 5 * timer_rate # every 5 seconds
 
-        # self.rate = 1.0  # Hz
-        # self.qos_profile = QoSProfile(qos_preset=QoSPreset.SENSOR_DATA)  # Adjust QoS parameters as needed
+        self.beep_srv = self.create_client(srv_type=Sound, srv_name="sound")
+        self.get_logger().info("Waiting for the /sound service...")
+        while not self.beep_srv.wait_for_service(timeout_sec=1.0):
+            print(".", end="")
+            sys.stdout.flush()
+        self.bringup_checks[2] = True
+        self.get_logger().info("/sound service is ready.")
 
-        self.beeper_pub = self.create_publisher(Sound, '/sound', qos_profile=10)
         self.battery_sub = self.create_subscription(
             BatteryState, '/battery_state', self.battery_callback, qos_profile=10)
         
@@ -53,7 +60,31 @@ class Tb3Status(Node):
 
     def timer_cb(self):
         self.check_active_nodes()
-        self.waffle_beeper()
+        now = self.get_clock().now()
+        if self.timer_counter > self.status_msg_trigger: 
+            msg_timestamp = dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            # runtime = (now - self.starttime).seconds_nanoseconds
+            # if runtime > 60:
+            #     runtimestring = f"{runtime / 60:.1f}"
+            #     units = "minutes"
+            # else:
+            #     runtimestring = f"{runtime:.0f}"
+            #     units = "seconds"
+            # # self.waffle_beeper(hush_if_ok=True)
+            self.get_logger().info(
+                f"\n\n{f'{msg_timestamp: ^21}':#^32}\n"
+                f"{'Device: ':>16}{hostname}\n"
+                f"{'Status: ':>16}{self.robot_status}\n"
+                f"{'Active Nodes: ':>16}{len(self.active_nodes)}\n"
+                # f"{'Up Time: ':>16}{runtimestring} {units}\n"
+                f"{'Battery: ':>16}{self.battery_voltage:.2f}V [{self.capacity}%]\n"
+            )
+            # self.timestamp = self.get_clock().now()
+            self.timer_counter = 0
+        else:
+            print(".", end="")
+            sys.stdout.flush()
+            self.timer_counter += 1
 
     def on_shutdown(self):
         self.get_logger().info(
@@ -70,94 +101,48 @@ class Tb3Status(Node):
         msg = topic_data.msg
         level = topic_data.level
 
-        if msg == "Run!" and node == "diff_drive_controller":
-            self.startup_complete[0] = True
+        if not self.bringup_complete:
+            if msg == "Run!" and node == "diff_drive_controller":
+                self.bringup_checks[0] = True
 
-        if msg == "Run!" and node == "turtlebot3_node":
-            self.startup_complete[1] = True
+            if msg == "Run!" and node == "turtlebot3_node":
+                self.bringup_checks[1] = True
+            
+            self.bringup_complete = all(self.bringup_checks)
+            if self.bringup_complete: 
+                if not self.waffle_core_errors:
+                    self.get_logger().info(
+                        "\n\n"
+                        "################################\n"
+                        f"{hostname} is up and running!\n"
+                        "################################\n"
+                    )
+                    self.bringup_errors = False
+                    self.waffle_beeper(value=1)
+                else:
+                    self.get_logger().error(
+                        "\n\n"
+                        "################################\n"
+                        "Bringup error, please try again.\n"
+                        "################################\n"
+                    )
+                    self.bringup_errors = True
 
-        if level > 2 and node in core_nodes and msg != "":
+        if level > 30 and node in core_nodes and msg != "":
+            # Any logs greter than WARN from any of the core nodes only:
             self.waffle_core_errors = True
+            self.robot_status = "ERROR"
+        else:
+            self.robot_status = "OK"
 
     def check_active_nodes(self):
         self.active_nodes = self.get_node_names()
-        core_nodes_up = all(node in self.active_nodes for node in core_nodes)
-        self.get_logger().info(
-            f"Node Check: {core_nodes_up}"
-        )
-        self.get_logger().info(
-            f"startup status: {self.startup_complete}"
-        )
-        if all(self.startup_complete):
-        
-            self.get_logger().info(
-                f"{'Battery: ':>16}{self.battery_voltage:.2f}V [{self.capacity}%]"
-            )
-        return core_nodes_up
+        return all(node in self.active_nodes for node in core_nodes)
 
-    # def print_status_msg(self):
-    #     now = self.get_clock().now()
-    #     if (now - self.timestamp).nanoseconds() > 5e9:  # Check every 5 seconds
-    #         msg_timestamp = dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    #         runtime = (now - self.starttime).seconds
-    #         if runtime > 60:
-    #             runtimestring = f"{runtime / 60:.1f}"
-    #             units = "minutes"
-    #         else:
-    #             runtimestring = f"{runtime:.0f}"
-    #             units = "seconds"
-    #         self.waffle_beeper(hush_if_ok=True)
-    #         os.system('clear')
-    #         print(f"{f'{msg_timestamp: ^21}':#^32}")
-    #         print(
-    #             f"{'Device: ':>16}{hostname}\n"
-    #             f"{'Status: ':>16}{self.robot_status_string}\n"
-    #             f"{'Active Nodes: ':>16}{len(self.active_nodes)}\n"
-    #             f"{'Up Time: ':>16}{runtimestring} {units}\n"
-    #             f"{'Battery: ':>16}{self.battery_voltage:.2f}V [{self.capacity}%]"
-    #         )
-    #         self.timestamp = self.get_clock().now()
-    #     else:
-    #         print(".", end="")
-    #         sys.stdout.flush()
-
-    def waffle_beeper(self):
-        # Beeping doesn't seem to work
-        # (not using ros2 topic pub either...?)
-        if not self.startup_sound and all(self.startup_complete):
-            self.get_logger().info(
-                "BEEPING!"
-            )
-            msg = Sound()
-            msg.value = 1
-            self.beeper_pub.publish(msg)
-            self.startup_sound = True
-        else:
-            self.get_logger().info(
-                "(Not Beeping)"
-            )
-
-    # def main(self):
-    #     while not self.startup_complete:
-    #         continue
-
-    #     bringup_errors = False
-    #     if self.waffle_beeper(hush_if_ok=False):
-    #         self.get_logger().info("--------------------------")
-    #         self.get_logger().info(f"{hostname} is up and running!")
-    #         self.get_logger().info("--------------------------")
-    #     else:
-    #         self.get_logger().error("--------------------------")
-    #         self.get_logger().error(f"{hostname} failed to launch correctly, please try again.")
-    #         self.get_logger().error("--------------------------")
-    #         bringup_errors = True
-
-    #     if not bringup_errors:
-    #         self.timestamp = self.get_clock().now()
-    #         rclpy.spin(self)
-    #     else:
-    #         rclpy.shutdown()
-
+    def waffle_beeper(self, value = 4):
+        msg = Sound.Request()
+        msg.value = value
+        self.beep_srv.call_async(msg)
 
 def main(args=None):
     rclpy.init(
