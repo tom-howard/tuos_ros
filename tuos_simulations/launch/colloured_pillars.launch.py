0488177011462d6224bc4938b57d11d83b1624db
Tom Howard <t.howard@sheffield.ac.uk>
2025-01-15 14:07:23 +0000

    standalone launch file

---
+++ tuos_simulations/launch/colloured_pillars.launch.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+"""
+Launch file tips:
+ - https://github.com/MetroRobots/rosetta_launch#05---set-a-command-line-argument
+"""
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+
+    tb3_gz_dir = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    gz_ros = get_package_share_directory('gazebo_ros')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+
+    world = os.path.join(
+        get_package_share_directory('tuos_simulations'),
+        'worlds',
+        'colloured_pillars.world'
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'with_gui', 
+            description="Select whether to launch Gazebo with or without Gazebo Client (i.e. the GUI).",
+            default_value='false'
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    gz_ros,
+                    'launch',
+                    'gzserver.launch.py'
+                )
+            ),
+            launch_arguments={'world': world}.items(),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    gz_ros,
+                    'launch',
+                    'gzclient.launch.py'
+                )
+            ),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    tb3_gz_dir,
+                    'robot_state_publisher.launch.py'
+                )
+            ),
+            launch_arguments={'use_sim_time': use_sim_time}.items()
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    tb3_gz_dir,
+                    'spawn_turtlebot3.launch.py'
+                )
+            ),
+            launch_arguments={
+                'x_pose': x_pose,
+                'y_pose': y_pose
+            }.items()
+        )
+    ])
+    
