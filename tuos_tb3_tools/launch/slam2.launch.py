754bf300dcb3773f244013533268a7297c555271
Tom Howard <t.howard@sheffield.ac.uk>
2025-10-15 15:07:56 +0000

    A common slam launch for real & sim envs (needs testing)

---
+++ tuos_tb3_tools/launch/slam2.launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+import os
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration, EqualsSubstitution, TextSubstitution
+from launch.conditions import IfCondition, LaunchConfigurationEquals
+
+def generate_launch_description():
+
+    this_launch_dir = os.path.join(
+        get_package_share_directory("tuos_tb3_tools"),
+        "launch"
+    )
+
+    environ = LaunchConfiguration('environment', default='real')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'environment', 
+            description="Define the operating environment: 'real' or 'sim'",
+            default_value='real'
+        ),
+        IncludeLaunchDescription( 
+            PythonLaunchDescriptionSource( 
+                os.path.join( 
+                    this_launch_dir, 
+                    "slam_real.launch.py" 
+                )
+            ),
+            condition=IfCondition(
+                EqualsSubstitution(environ, TextSubstitution(text='real'))
+            )
+        ),
+        IncludeLaunchDescription( 
+            PythonLaunchDescriptionSource( 
+                os.path.join( 
+                    this_launch_dir, 
+                    "slam_sim.launch.py" 
+                )
+            ),
+            condition=IfCondition(
+                EqualsSubstitution(environ, TextSubstitution(text='sim'))
+            )
+        ),
+
+    ])
