f9bbc592c9da01820972f4d2053635ae9cba89f2
Tom Howard <t.howard@sheffield.ac.uk>
2025-10-22 15:53:57 +0000

    Line following worlds updated

--- tuos_simulations/launch/line_following.launch.py
+++ tuos_simulations/launch/line_following.launch.py
@@ -7,27 +7,25 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 
 def generate_launch_description():
 
-    tb3_gz_dir = os.path.join(
-        get_package_share_directory('turtlebot3_gazebo'), 'launch')
-    gz_ros = get_package_share_directory('gazebo_ros')
-    this_launch_dir = os.path.join(
-        get_package_share_directory('tuos_simulations'), 'launch')
-
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    x_pose = LaunchConfiguration('x_pose', default='2.4')
-    y_pose = LaunchConfiguration('y_pose', default='-2.5')
-    yaw = LaunchConfiguration('yaw', default='3.14')
-    with_gui = LaunchConfiguration('with_gui', default='true')
-
-    world = os.path.join(
-        get_package_share_directory('tuos_simulations'),
-        'worlds',
+    gz_ros = os.path.join(
+        get_package_share_directory('ros_gz_sim'), 'launch'
+    )
+    tuning_world = os.path.join(
+        get_package_share_directory('tuos_simulations'), 
+        'worlds', 
+        'line_following_setup.world'
+    )
+    line_following_world = os.path.join(
+        get_package_share_directory('tuos_simulations'), 
+        'worlds', 
         'line_following.world'
     )
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -35,30 +33,52 @@ def generate_launch_description():
             description="Select whether to launch Gazebo with or without Gazebo Client (i.e. the GUI).",
             default_value='true'
         ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(
-                    gz_ros,
-                    'launch',
-                    'gzserver.launch.py'
-                )
-            ),
-            launch_arguments={'world': world}.items(),
+        DeclareLaunchArgument(
+            'tuning', 
+            description="Select whether to launch a world designed for PID tuning (following a straight line) or the standard line following world (tuning:=false).",
+            default_value='false'
         ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(
                     gz_ros,
-                    'launch',
-                    'gzclient.launch.py'
+                    'gz_sim.launch.py'
                 )
             ),
-            condition=IfCondition(with_gui)
+            launch_arguments={'gz_args': ['-r -s -v1 ', tuning_world], 'on_exit_shutdown': 'true'}.items(),
+            condition = IfCondition(
+                LaunchConfiguration('tuning')
+            )
         ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(
-                    tb3_gz_dir,
+                    gz_ros,
+                    'gz_sim.launch.py'
+                )
+            ),
+            launch_arguments={'gz_args': ['-r -s -v1 ', line_following_world], 'on_exit_shutdown': 'true'}.items(),
+            condition = UnlessCondition(
+                LaunchConfiguration('tuning')
+            )
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    gz_ros,
+                    'gz_sim.launch.py'
+                )
+            ),
+            launch_arguments={'gz_args': '-g -v1 '}.items(),
+            condition=IfCondition(
+                LaunchConfiguration('with_gui')
+            )
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('turtlebot3_gazebo'), 
+                    'launch',
                     'robot_state_publisher.launch.py'
                 )
             ),
@@ -67,15 +87,35 @@ def generate_launch_description():
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(
-                    this_launch_dir,
+                    get_package_share_directory('tuos_simulations'), 
+                    'launch', 
                     'spawn_tb3.launch.py'
                 )
             ),
             launch_arguments={
-                'x_pose': x_pose,
-                'y_pose': y_pose,
-                'yaw': yaw
-            }.items()
+                'x_pose': LaunchConfiguration('x_pose', default='0.5'),
+                'y_pose': LaunchConfiguration('y_pose', default='-5.48'),
+                'yaw': LaunchConfiguration('yaw', default='1.57'),
+            }.items(),
+            condition=IfCondition(
+                LaunchConfiguration('tuning')
+            )
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('tuos_simulations'), 
+                    'launch', 
+                    'spawn_tb3.launch.py'
+                )
+            ),
+            launch_arguments={
+                'x_pose': LaunchConfiguration('x_pose', default='-2.4'),
+                'y_pose': LaunchConfiguration('y_pose', default='-2.5'),
+                'yaw': LaunchConfiguration('yaw', default='1.57'),
+            }.items(),
+            condition=UnlessCondition(
+                LaunchConfiguration('tuning')
+            )
         )
     ])
-    
